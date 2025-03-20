028a4829878dbee1d960bf8b21ad9db5eeaa1202
Sudha Sravya <sudha.s@gormalone.com>
2025-03-20 13:26:10 -0500

    Updated activity_main.xml.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.fetchassessment"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.fetchassessment"
